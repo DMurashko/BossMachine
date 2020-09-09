58ca2de45105aa2caaf43774fb8bcb234150ae74
DMurashko <appleitunesstore590@gmail.com>
2020-09-09 21:44:47 +0300

    New GET request added, GET requests are pending for curl tests

--- server/api.js
+++ server/api.js
@@ -21,8 +21,17 @@ apiRouter.get('/minions', (req, res, next) => {
 
 apiRouter.post('/minions', (req, res, next) => {
 	if (req.body) {
-		addToDatabase('minions', req.body);
+		let mins = addToDatabase('minions', req.body);
 		res.status(201).send(mins);
+	} else {
+		res.status(404).send();
+	}
+});
+
+apiRouter.get('/minions/:minionId', (req, res, next) => {
+	let minion = getFromDatabaseById('minions', req.params.minionId);
+	if (minion) {
+		res.send(minion);
 	} else {
 		res.status(404).send();
 	}
@@ -32,6 +41,15 @@ apiRouter.get('/ideas', (req, res, next) => {
 	let mins = getAllFromDatabase('ideas');
 	if (mins) {
 		res.send(mins);
+	} else {
+		res.status(404).send();
+	}
+});
+
+apiRouter.get('/ideas/:ideaId', (req, res, next) => {
+	let idea = getFromDatabaseById('ideas', req.params.ideaId);
+	if (idea) {
+		res.send(idea);
 	} else {
 		res.status(404).send();
 	}
